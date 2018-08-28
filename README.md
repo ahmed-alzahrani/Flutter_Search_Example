2dfbb04ec2c8f92a0245dfaf4314c1a67e879471
ahmed-alzahrani <ahmedalzahrani94@gmail.com>
2018-08-28 17:10:54 -0400

    adding example from local

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Flutter_Search_Example
-An example Flutter application to show users how to query an API for info, display the result of the query into a List View, and also implement a search bar at the top of the app.
+# flutter_api_filter_list
+
+A new Flutter demo application built to show people an easy way to do the following:
+
+- Query an API for some info using flutter package dio
+
+- Create a page that generates a listView for the user based on the info retrieved from the API
+
+- Add a search bar in the top App Bar where the user can filter their listView
+
+Below is an example of the search bar in action:
+
+![gif](example.gif)
---
+++ lib/main.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Api Filter list Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new ExamplePage(),
+    );
+  }
+}
+
+class ExamplePage extends StatefulWidget {
+  ExamplePage({ Key key }) : super(key: key);
+  @override
+  _ExamplePageState createState() => new _ExamplePageState();
+}
+
+class _ExamplePageState extends State<ExamplePage> {
+  final formKey = new GlobalKey<FormState>();
+  final key = new GlobalKey<ScaffoldState>();
+  final TextEditingController _filter = new TextEditingController();
+  final dio = new Dio();
+  String _searchText = "";
+  List names = new List();
+  List filteredNames = new List();
+  Icon _searchIcon = new Icon(Icons.search);
+  Widget _appBarTitle = new Text( 'Search Example' );
+
+  _ExamplePageState() {
+    _filter.addListener(() {
+      if (_filter.text.isEmpty) {
+        setState(() {
+          _searchText = "";
+          filteredNames = names;
+        });
+      } else {
+        setState(() {
+          _searchText = _filter.text;
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    this._getNames();
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildBar(context),
+      body: Container(
+        child: _buildList(),
+      ),
+      resizeToAvoidBottomPadding: false,
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      centerTitle: true,
+      title: _appBarTitle,
+      leading: new IconButton(
+        icon: _searchIcon,
+        onPressed: _searchPressed,
+
+      ),
+    );
+  }
+
+  Widget _buildList() {
+    if (!(_searchText.isEmpty)) {
+      List tempList = new List();
+      for (int i = 0; i < filteredNames.length; i++) {
+        if (filteredNames[i]['name'].toLowerCase().contains(_searchText.toLowerCase())) {
+          tempList.add(filteredNames[i]);
+        }
+      }
+      filteredNames = tempList;
+    }
+    return ListView.builder(
+      itemCount: names == null ? 0 : filteredNames.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new ListTile(
+          title: Text(filteredNames[index]['name']),
+          onTap: () => print(filteredNames[index]['name']),
+        );
+      },
+    );
+  }
+
+  void _searchPressed() {
+    setState(() {
+      if (this._searchIcon.icon == Icons.search) {
+        this._searchIcon = new Icon(Icons.close);
+        this._appBarTitle = new TextField(
+          controller: _filter,
+          decoration: new InputDecoration(
+            prefixIcon: new Icon(Icons.search),
+            hintText: 'Search...'
+          ),
+        );
+      } else {
+        this._searchIcon = new Icon(Icons.search);
+        this._appBarTitle = new Text( 'Search Example' );
+        filteredNames = names;
+        _filter.clear();
+      }
+    });
+  }
+
+  void _getNames() async {
+    final response = await dio.get('https://swapi.co/api/people');
+    List tempList = new List();
+    for (int i = 0; i < response.data['results'].length; i++) {
+      tempList.add(response.data['results'][i]);
+    }
+    setState(() {
+      names = tempList;
+      names.shuffle();
+      filteredNames = names;
+    });
+  }
+
+
+}
